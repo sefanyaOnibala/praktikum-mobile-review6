db9526c654574a3de616c5fa7e973800a54302dd
sefanyaOnibala <lomeyonibala@gmail.com>
2024-10-23 11:25:18 +0800

    Review6

---
+++ android/app/src/main/kotlin/com/example/review_6/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.review_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widgets/task_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:review_6/model/task_data.dart';
+import 'package:review_6/widgets/task_tile.dart';
+
+import '../model/task.dart';
+
+class TasksList extends StatelessWidget {
+  const TasksList({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.tasks.length,
+        );
+      },
+    );
+  }
+}
